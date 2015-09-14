f7f477857a7922c3e0d98990124b70616a0a98c1
Pravin Kandala <z1761257@DoITs-Mac-mini-2.local>
2015-09-14 09:33:40 -0500

    prints message when clicked the example button

--- gitUseExample/ViewController.swift
+++ gitUseExample/ViewController.swift
@@ -14,12 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickTheButton(sender: AnyObject) {
+        println("hello world")
+    }
 
 }
 
