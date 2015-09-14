b61317a017e84a2881afaab10e2320b24384a3f2
Pravin Kandala <z1761257@DoITs-Mac-mini-2.local>
2015-09-14 09:39:19 -0500

    edit mapforitenary class

---
+++ gitUseExample/MapForItenary.swift
@@ -0,0 +1,14 @@
+//
+//  MapForItenary.swift
+//  gitUseExample
+//
+//  Created by Pravin Kandala on 9/14/15.
+//  Copyright (c) 2015 Pravin Kandala. All rights reserved.
+//
+
+import Foundation
+
+class MapForItenary {
+
+    
+}
